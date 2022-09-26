f48505f480ab8c0c54b530512f94088ec774bdb8
harseille <joonhabaak@gmail.com>
2022-09-26 19:14:37 +0900

    Section 15

---
+++ react-custom-hook-2/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+import useHttp from './hooks/use-http';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const httpData = useHttp();
+
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: 'https://react-http-playground-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ react-custom-hook-2/src/components/NewTask/NewTask.js
@@ -0,0 +1,48 @@
+import useHttp from '../../hooks/use-http';
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequset } = useHttp();
+
+  // * 중첩 구조를 피하기 위해 위한 방법
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    // * 중첩 구조
+    // const createTask = (taskData) => {
+    //   const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    //   const createdTask = { id: generatedId, text: taskText };
+
+    //   props.onAddTask(createdTask);
+    // };
+
+    sendTaskRequset(
+      {
+        url: 'https://react-http-playground-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      // createTask
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
